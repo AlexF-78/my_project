c2c4e2b116422930aa8a1e91b6e433e80d8acd42
алексей федулов <fab-78@mail.ru>
2025-05-17 21:35:26 +0200

    beginning

---
+++ src/masks.py
@@ -0,0 +1,35 @@
+def get_mask_card_number(card_number: str) -> str:
+    """
+    Маскирует номер карты в формате XXXX XX** **** XXXX.
+    где X - цифры номера карты
+    """
+
+    # Первые 6 цифр
+    first_numbers = card_number[:6]
+    # Последние 4 цифры
+    recent_numbers = card_number[-4:]
+
+    # Заменяем остальные цифры на звездочки
+    masked_numbers = "****"
+
+    # Формируем маскированный номер
+    masked_card_number = f"{first_numbers} {masked_numbers} {recent_numbers}"
+
+    return masked_card_number
+
+
+def get_mask_account(account_number: str) -> str:
+    """
+    Маскирует номер счета в формате **XXXX.
+    где X - цифры номера счета .
+
+    """
+
+    # Получаем последние 4 цифры
+    visible_end = account_number[-4:]
+
+    # Формируем мас
+    # кированный номер
+    masked_account_number = f"**{visible_end}"
+
+    return masked_account_number
