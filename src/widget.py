12428630f43cc00b10f3cfa0f08d2384fc7a0c34
алексей федулов <fab-78@mail.ru>
2025-05-18 23:36:01 +0200

    formatting

--- src/widget.py
+++ src/widget.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from masks import get_mask_card_number, get_mask_account
 
 
-def mask_account_card (info: str) -> str:
+def mask_account_card(info: str) -> str:
     """
     Функция маскировки информации карты или счета
     """
@@ -13,18 +13,17 @@ def mask_account_card (info: str) -> str:
     # определяем тип по первому слову
     first_word = parts[0]
     if first_word.lower() == "счет":
-        #Обработка счета
+        # Обработка счета
         score_number = parts[1]
         masked = get_mask_account(score_number)
         return f"Счет {masked}"
     else:
         digit_card = parts[-1]
-        card_type = " ". join(parts[:-1])
+        card_type = " ".join(parts[:-1])
         masked_number = get_mask_card_number(digit_card)
         return f"{card_type} {masked_number}"
+
 
 def get_date(date_str: str) -> str:
     dt = datetime.fromisoformat(date_str)
     return dt.strftime("%d.%m.%Y")
-
-
