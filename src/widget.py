5e8e142c56ea5fbaefac50884096da6faee35070
алексей федулов <fab-78@mail.ru>
2025-05-18 22:50:19 +0200

    mask_account_card

---
+++ src/widget.py
@@ -0,0 +1,25 @@
+from masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card (info: str) -> str:
+    """
+    Функция маскировки информации карты или счета
+    """
+    parts = info.split()
+    if not parts:
+        return info
+
+    # определяем тип по первому слову
+    first_word = parts[0]
+    if first_word.lower() == "счет":
+        #Обработка счета
+        score_number = parts[1]
+        masked = get_mask_account(score_number)
+        return f"Счет {masked}"
+    else:
+        digit_card = parts[-1]
+        card_type = " ". join(parts[:-1])
+        masked_number = get_mask_card_number(digit_card)
+        return f"{card_type} {masked_number}"
+
+
