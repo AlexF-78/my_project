b2ea740f0df3ec65dc9dead7a0f6106bf1a7514c
алексей федулов <fab-78@mail.ru>
2025-05-19 21:35:47 +0200

    correction

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from masks import get_mask_card_number, get_mask_account
+from .masks import get_mask_card_number, get_mask_account
 
 
 def mask_account_card(info: str) -> str:
@@ -28,3 +28,12 @@ def get_date(date_str: str) -> str:
     """Форматирует дату"""
     dt = datetime.fromisoformat(date_str)
     return dt.strftime("%d.%m.%Y")
+
+print(mask_account_card("Visa Platinum 7000792289606361"))
+# Вывод: Visa Platinum 7000 79** **** 6361
+
+print(mask_account_card("Счет 73654108430135874305"))
+# Вывод: Счет **4305
+
+print(get_date("2024-03-11T02:26:18.671407"))
+# Вывод: 11.03.2024
