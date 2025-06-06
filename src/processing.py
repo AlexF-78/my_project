e7a8c398816359402bb6e835b69d4ef40c77c3d1
алексей федулов <fab-78@mail.ru>
2025-06-07 00:27:44 +0200

    corrected widget.py

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,4 @@
-from typing import Dict, Any
+from typing import Any, Dict
 
 
 def filter_by_state(records: list[dict], state: str = 'EXECUTED') -> list[dict]:
--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-from .masks import get_mask_card_number, get_mask_account
+
+from .masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(info: str) -> str:
     """
@@ -10,8 +12,8 @@ def mask_account_card(info: str) -> str:
         return info
     first_word = parts[0]
     if first_word.lower() == "счет":
-        if len(parts) < 2:
-            return info  # Нет номера счета
+        if len(parts) != 16:
+            raise ValueError('Invalid parts number')
         score_number = parts[1]
         masked = get_mask_account(score_number)
         return f"Счет {masked}"
@@ -23,10 +25,11 @@ def mask_account_card(info: str) -> str:
         masked_number = get_mask_card_number(digit_card)
         return f"{card_type} {masked_number}"
 
+
 def get_date(date_str: str) -> str:
     """Форматирует дату из ISO-формата в дд.мм.гггг."""
     try:
         dt = datetime.fromisoformat(date_str)
         return dt.strftime("%d.%m.%Y")
     except ValueError:
-        return date_str  # Возвращает исходную строку при ошибке
+        raise ValueError('Invalid data')
