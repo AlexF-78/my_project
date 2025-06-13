0bd6f2173c38814d9937d4cd72af7d634da5fcd2
алексей федулов <fab-78@mail.ru>
2025-06-14 00:20:07 +0200

    typing

--- src/generators.py
+++ src/generators.py
@@ -1,4 +1,13 @@
-def filter_by_currency(transactions, currency_code):
+from typing import Any, Dict, Generator, List
+
+
+def filter_by_currency(
+    transactions: List[Dict[str, Any]],
+    currency_code: str
+) -> Generator[Dict[str, Any], None, None]:
+    """
+    Генерирует транзакции из списка, у которых валюта совпадает с заданным кодом.
+    """
     for transaction in transactions:
         # Проверяем, что у транзакции есть нужные данные
         operation_amount = transaction.get("operationAmount")
@@ -20,26 +29,28 @@ def filter_by_currency(transactions, currency_code):
             yield transaction
 
 
-def transaction_descriptions(transactions):
+def transaction_descriptions(
+    transactions: List[Dict[str, Any]]
+) -> Generator[str, None, None]:
     """
     Генератор, который по очереди возвращает описание каждой транзакции из списка.
     """
     for transaction in transactions:
-        description = transaction.get('description', '').lower()
+        description = transaction.get("description", "").lower()
 
         # Варианты описаний
-        if 'перевод организации' in description:
-            yield 'Перевод организации'
-        elif 'перевод со счета на счет' in description:
-            yield 'Перевод со счета на счет'
-        elif 'перевод с карты на карту' in description:
-            yield 'Перевод с карты на карту'
+        if "перевод организации" in description:
+            yield "Перевод организации"
+        elif "перевод со счета на счет" in description:
+            yield "Перевод со счета на счет"
+        elif "перевод с карты на карту" in description:
+            yield "Перевод с карты на карту"
         else:
             # Для любых других случаев
-            yield description.capitalize() or 'Описание не указано'
+            yield description.capitalize() or "Описание не указано"
 
 
-def card_number_generator(start, end):
+def card_number_generator(start: int, end: int) -> Generator[str, None, None]:
     """
     Генератор номеров банковских карт в формате 'XXXX XXXX XXXX XXXX'.
     """
@@ -47,5 +58,5 @@ def card_number_generator(start, end):
         # Форматируем число с ведущими нулями до 16 цифр
         card_number = str(number).zfill(16)
         # Разбиваем на группы по 4 цифры
-        formatted_number = ' '.join([card_number[i:i+4] for i in range(0, 16, 4)])
+        formatted_number = " ".join([card_number[i: i + 4] for i in range(0, 16, 4)])
         yield formatted_number
