31c8aacd5e77daba39ef11f2268c865b4919e3e0
алексей федулов <fab-78@mail.ru>
2025-06-13 13:36:05 +0200

    transaction_descriptions in generators

--- src/generators.py
+++ src/generators.py
@@ -18,3 +18,22 @@ def filter_by_currency(transactions, currency_code):
 
         if code == currency_code:
             yield transaction
+
+
+def transaction_descriptions(transactions):
+    """
+    Генератор, который по очереди возвращает описание каждой транзакции из списка.
+    """
+    for transaction in transactions:
+        description = transaction.get('description', '').lower()
+
+        # Варианты описаний
+        if 'перевод организации' in description:
+            yield 'Перевод организации'
+        elif 'перевод со счета на счет' in description:
+            yield 'Перевод со счета на счет'
+        elif 'перевод с карты на карту' in description:
+            yield 'Перевод с карты на карту'
+        else:
+            # Для любых других случаев
+            yield description.capitalize() or 'Описание не указано'
